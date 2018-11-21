d958535b29cf603d8b147f86b80002e826c55a26
D. Wilcox <dw@angk.org>
2018-11-21 12:59:37 -0600

    Test component

---
+++ src/components/HcTest.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-btn
+     :color="color"
+     flat
+     >
+    This is a test
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'hc-test',
+  props: {
+    color: {
+      type: String,
+      default: 'primary'
+    }
+  }
+}
+</script>
--- src/plugin.js
+++ src/plugin.js
@@ -1,9 +1,34 @@
 import Vue from 'vue'
+import upperFirst from 'lodash/upperFirst'
+import camelCase from 'lodash/camelCase'
 import Vuetify from 'vuetify/lib'
 import 'vuetify/src/stylus/app.styl'
 import { theme } from './styles'
 
+const requireComponent = require.context(
+  './components',
+  true,
+  /\w+\.(vue|js)$/
+)
+
 Vue.use(Vuetify, {
   iconfont: 'md',
   theme
+})
+
+requireComponent.keys().forEach(filename => {
+  const componentConfig = requireComponent(filename)
+
+  const componentName = upperFirst(
+    camelCase(
+      // Strip the leading `./` and extension from the filename
+      fileName.replace(/^\.\/(.*)\.\w+$/, '$1')
+    )
+  )
+
+  // Register component globally
+  Vue.component(
+    componentName,
+    componentConfig.default || componentConfig
+  )
 })
