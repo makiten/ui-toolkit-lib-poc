182143c22184d895d9e533d925790d431a6004df
Donald Wilcox <donald.wilcox@harlandclarke.com>
2018-11-28 17:40:44 -0600

    New footer

---
+++ src/components/UtkFooter.vue
@@ -0,0 +1,62 @@
+<template>
+  <v-footer
+    :color="color"
+    :app="app"
+  >
+    <v-layout
+      justify-center
+      row
+      wrap
+    >
+      <v-flex
+        xs12
+        md2
+      >
+        &copy; {{ copyrightYear }}
+      </v-flex>
+      <v-btn
+        v-for="link in links"
+        :key="link.text"
+        :color="linkColor"
+        :to="link.href"
+        flat
+      >
+        {{ link.text }}
+      </v-btn>
+    </v-layout>
+
+  </v-footer>
+</template>
+
+<script>
+  export default {
+    name: 'UtkFooter',
+    props: {
+      app: {
+        type: Boolean,
+        default: false
+      },
+      color: {
+        type: String,
+        default: null
+      },
+      copyrightYear: {
+        type: Number,
+        default: () => (new Date()).getFullYear()
+      },
+      links: {
+        type: Array,
+        default: () => []
+      },
+      linkColor: {
+        type: String,
+        default: 'white'
+      }
+    },
+    data: () => ({})
+  }
+</script>
+
+<style scoped>
+
+</style>
